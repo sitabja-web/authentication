83e731337fccef26edda1c4b38a4df98130bd110
sitabja biswas <sitabjabiswas633@gmail.com>
2026-08-22 21:10:47 +0530

    new files

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,27 +1,29 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-       username:{
-              type:String,
-              required: [true, "username is required"],
-              unique: [ true, "username must be unique"]
-       },
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true
+    },
 
-       email: {
-              type: String,
-              required: [true , "Email is required"],
-              uniquw: [true , "email must be unique"]
-       },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
 
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
 
-       password: {
-              type: String,
-              required: [true,  "password is required"]
-       },
-})
+    verified: {
+        type: Boolean,
+        default: false
+    }
+});
 
+const userModel = mongoose.model("User", userSchema);
 
-
-const  userModel   = mongoose.model("User", userSchema)
-
-export default  userModel
+export default userModel;h
